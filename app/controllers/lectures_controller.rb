62b68ed7e307f0de3d0d67fb5865d16c48a6f105
Ryo <matuoka2096@gmail.com>
2021-02-09 23:01:33 +0900

    講義モデル表示順序変更

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -1,6 +1,7 @@
 class LecturesController < ApplicationController
   def index
     @q = Lecture.ransack(params[:q])
+    @q.sorts = 'updated_at desc' if @q.sorts.empty?
     @lectures = @q.result.page(params[:page]).per(25)
   end
 
