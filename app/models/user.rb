063d783dabf293290f04518b7381a5b515795cb7
kenji132 <ue52483@gmail.com>
2021-02-10 15:38:32 +0900

    devise修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :name, presence: true, uniqueness: true, length: {  maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },format: { with: VALID_EMAIL_REGEX },uniqueness: true
+  validates :email, length: { maximum: 255 },format: { with: VALID_EMAIL_REGEX }
+  validates :name, presence: true, length: { minimum: 2, maximum: 20}
 end
