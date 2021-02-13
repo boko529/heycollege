63f125d7f05de0a7527b7163c2ebca44a4510e5b
kenji132 <ue52483@gmail.com>
2021-02-13 18:04:16 +0900

    userのshow,edit,

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
   root 'static_pages#home'
   resources :lectures
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,46 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Warden::Test::Helpers
+  def setup
+    @user = users(:user3)
+    @other_user = users(:user4)
+  end
+
+  test "invalid user should not edit valid user's name" do
+    login_as(@other_user, scope: :user)
+    get edit_user_path(@user.id)
+    assert_redirected_to user_path(@other_user)
+  end
+  
+  test "valid user should edit invalid user's name" do
+    login_as(@user, scope: :user)
+    get edit_user_path(@user.id)
+    patch user_path(@user.id), params: { user: { name:  ""}}
+    assert_template 'users/edit'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert.alert-danger'
+  end
+  
+  test "valid user should edit valid user's name" do
+    login_as(@user, scope: :user)
+    get edit_user_path(@user.id)
+    patch user_path(@user.id), params: { user: { name:  "taro"}}
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "div.alert"
+  end
+  
+  test "don't login user should not edit other user's name" do
+    get edit_user_path(@user.id)
+    follow_redirect!
+    assert_template 'devise/sessions/new'
+    assert_select "div.alert"
+  end
+
+
+  test "should get show" do
+    get user_path(@user.id)
+    assert_response :success
+  end
 end
