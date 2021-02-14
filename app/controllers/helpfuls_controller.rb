95b78ed5f6229d033740b0be75a5a20663be42af
Ryo <matuoka2096@gmail.com>
2021-02-14 23:07:45 +0900

    役に立った機能Ajax化完了

--- app/controllers/helpfuls_controller.rb
+++ app/controllers/helpfuls_controller.rb
@@ -1,13 +1,21 @@
 class HelpfulsController < ApplicationController
+  before_action :set_review, only: [:create, :destroy]
+  before_action :set_lecture, only: [:create, :destroy]
   def create
     helpful = current_user.helpfuls.build(review_id: params[:review_id])
     helpful.save
-    redirect_to lectures_path
   end
 
   def destroy
-    helpful = Helpful.find_by(review_id: params[:review_id], user_id: current_user.id)
+    helpful = Helpful.find_by(user_id: current_user.id, review_id: params[:review_id] )
     helpful.destroy
-    redirect_to lectures_path
   end
+
+  private
+    def set_review
+      @review = Review.find_by(id: params[:review_id])
+    end
+    def set_lecture
+      @lecture = Lecture.find_by(id: params[:lecture_id])
+    end
 end
