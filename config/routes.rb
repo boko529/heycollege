a9c6b43be4111b675edd98d6ec080780c77cf9b6
kenji132 <ue52483@gmail.com>
2021-02-17 20:51:49 +0900

    管理者機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users
+  resources :users 
+  namespace :admin do
+  resources :users, only: [:index]
+  end
   root 'static_pages#home'
   resources :lectures
 end
