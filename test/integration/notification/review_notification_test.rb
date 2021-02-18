ba40476bede088a69f6602937f0abd508961b167
Ryo <matuoka2096@gmail.com>
2021-02-18 23:47:23 +0900

    インテグレーションテストまで

--- test/integration/notification/review_notification_test.rb
+++ test/integration/notification/review_notification_test.rb
@@ -6,7 +6,6 @@ class ReviewNotificationTest < ActionDispatch::IntegrationTest
     @user = users(:user1)
     @other_user = users(:user2)
     @review = reviews(:review1)
-    @notification = Notification.new(visitor_id: @other_user, visited_id: @review.user, review_id: @review.id)
   end
 
   test "display circle when new notification" do
@@ -15,8 +14,16 @@ class ReviewNotificationTest < ActionDispatch::IntegrationTest
     assert_template 'static_pages/home'
     assert_select 'i.fas.fa-circle.n-circle.fa-2x', 0
     assert_difference 'Notification.count', 1 do
-      @notification.save
+      Notification.create!(visitor_id: @other_user.id, visited_id: @review.user.id, review_id: @review.id)
     end
+    # ページの再読み込み(リロード)をしています
+    get root_path
+    assert_template 'static_pages/home'
+    #新着メッセージのオレンジ色の丸の確認
     assert_select 'i.fas.fa-circle.n-circle.fa-2x', 1
+    # お知らせ一覧に移動、オレンジ色の丸が消えていることを確認
+    get notifications_path
+    assert_template "notifications/index"
+    assert_select 'i.fas.fa-circle.n-circle.fa-2x', 0
   end
 end
