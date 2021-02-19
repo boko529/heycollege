dbe647e9ab87a3fd6d7bef1409a0d8238b1b1e7b
Ryo <matuoka2096@gmail.com>
2021-02-19 11:35:23 +0900

    参考になるに対しての通知機能

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,4 +1,5 @@
 class NotificationsController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
   def index
     @notifications = current_user.passive_notifications.page(params[:page]).per(20)
     # 未確認だった通知に対して開いたら既読済みに変更
--- test/controllers/notifications_controller_test.rb
+++ test/controllers/notifications_controller_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
-
 class NotificationsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Warden::Test::Helpers
+  def setup
+    @user1 = users(:user1)
+    @user2 = users(:user2)
+    @lecture = lectures(:lecture_1)
+    @review = reviews(:review1)
+  end
+
+  test "notification index" do
+    login_as(@user1, scope: :user)
+    get notifications_path
+    assert_template "notifications/index"
+  end
+
+  #ログインしてないと弾かれる
+  test "notification index not login" do
+    get notifications_path
+    assert_template nil
+  end
 end
