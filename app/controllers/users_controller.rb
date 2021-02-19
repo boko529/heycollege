cc81e2eea8e05ca1e8f12277ebb20ed0bb421739
kenji132 <ue52483@gmail.com>
2021-02-19 15:20:21 +0900

    userのshowページなどの編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:edit,:update]
   def show
     @user = User.find(params[:id])
+    @reviews = @user.reviews
   end
 
   def edit
