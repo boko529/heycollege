f0711b061073188794957ce2d4b3495ddcd5418b
kenji132 <ue52483@gmail.com>
2021-02-21 10:57:17 +0900

    userの性別、学部、学年編集機能＆そのテスト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name)
+    params.require(:user).permit(:name,:gender,:grade,:faculty)
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -23,18 +23,24 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_select 'div.alert.alert-danger'
   end
   
-  test "valid user should edit valid user's name" do
+  test "valid user should edit valid user information" do
     login_as(@user, scope: :user)
     get edit_user_path(@user.id)
-    patch user_path(@user.id), params: { user: { name:  "taro"}}
+    patch user_path(@user.id), params: { user: { name:  "shouko"}}
+    patch user_path(@user.id), params: { user: { gender:  "female"}}
+    patch user_path(@user.id), params: { user: { grade:  "M1"}}
+    patch user_path(@user.id), params: { user: { faculty:  "APM"}}
     follow_redirect!
     assert_template 'users/show'
     @user.reload
-    assert_equal 'taro', @user.name
+    assert_equal 'shouko', @user.name
+    assert_equal 'female', @user.gender
+    assert_equal 'M1', @user.grade
+    assert_equal 'APM', @user.faculty
     assert_select "div.alert"
   end
   
-  test "don't login user should not edit other user's name" do
+  test "don't login user should not edit other user information" do
     get edit_user_path(@user.id)
     follow_redirect!
     assert_template 'devise/sessions/new'
@@ -46,9 +52,24 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     patch user_path(@other_user.id), params: { user: { name:  name } }
     assert_template 'users/edit'
   end
+  test "others gender faculty gender update" do
+    login_as(@user, scope: :user)
+    patch user_path(@other_user.id), params: { user: { gender:  "female" } }
+    patch user_path(@other_user.id), params: { user: { grade:  "M1" } }
+    patch user_path(@other_user.id), params: { user: { faculty: "APM" } }
+    follow_redirect!
+    assert_template 'users/show'
+  end
 
   test "update not log in" do
     patch user_path(@user.id), params: { user: { name: "taro" } }
+    assert_template nil
+  end
+
+  test "update gender faculty gender not log in" do
+    patch user_path(@other_user.id), params: { user: { gender:  "female" } }
+    patch user_path(@other_user.id), params: { user: { grade:  "M1" } }
+    patch user_path(@other_user.id), params: { user: { faculty: "APM" } }
     assert_template nil
   end
 
