ba8f61c3342efe3e860f08f0b7034bdf940cc69e
kenji132 <ue52483@gmail.com>
2021-02-23 14:23:32 +0900

    Userのメール認証テスト完了

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,9 +2,6 @@ require "test_helper"
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
   include Warden::Test::Helpers
-  def setup
-    @new_user = User.new(name: "ExampleUser", email: "user@example.com",password: "foobar",password_confirmation: "foobar", gender: "male", grade: "B1", faculty: "APS")
-  end
 
   test "invalid signup information password" do
     get new_user_registration_path
@@ -21,29 +18,35 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select "div#error_explanation"
   end
 
-  test "valid signup information" do
+  test "valid signup information and don't confirmed user" do
     get new_user_registration_path
     assert_difference 'User.count' ,1 do
       post user_registration_path, params: { user: { name:  "taro",
                                         email: "taro@example.com",
                                         password: "password",
                                         password_confirmation: "password",
-                                        confirmed_at: Time.now - 100,
+                                        gender: "male",
+                                        grade: "B1",
+                                        faculty:"APS" } }
+    end
+    assert_template 'devise/mailer/confirmation_instructions'
+    post user_session_path params: { session: { email: 'taro@example.com', password: 'password' } }
+    assert_template 'devise/sessions/new'
+  end
+  
+  test "valid signup information" do
+    get new_user_registration_path
+    assert_difference 'User.count' ,1 do
+      post user_registration_path, params: { user: { 
+                                        name:  "taro",
+                                        email: "taro@example.com",
+                                        password: "password",
+                                        password_confirmation: "password",
                                         gender: "male",
                                         grade: "B1",
                                         faculty:"APS" } }
     end
     get root_path
-    assert_template 'static_pages/home' 
     assert_select "div.alert"
-  end
-  
-  test "valid signup information2" do
-    @new_user = User.new(name: "ExampleUser", email: "user@example.com",password: "foobar",password_confirmation: "foobar", gender: "male", grade: "B1", faculty: "APS")
-    @new_user.skip_confirmation!
-    @new_user.save
-    get user_path(@new_user.id)
-    assert_template 'users/show' 
-    assert_select 'td', @new_user.name
   end
 end
