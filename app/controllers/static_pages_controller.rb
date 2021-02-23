e79ab9168162869403d9a1315695be5e3b341f6c
[Cota] <[kotarouh777@hotmail.com]>
2021-02-23 22:11:02 +0900

    homeに先生ランキング追加

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,5 +10,25 @@ class StaticPagesController < ApplicationController
     end.reverse
     @lectures = Kaminari.paginate_array(@lectures).page(params[:page]).per(20)
     @news = News.all
+
+    @teachers = Teacher.left_joins(:lectures).distinct.sort_by do |teacher|
+      sum = 0
+      count = 0
+      lectures = teacher.lectures do |lecture|
+        reviews = lecture.reviews
+        if reviews.present?
+          sum += reviews.map(&:score).sum / reviews.size
+          count += 1
+        else
+          0
+        end
+      end
+      if count == 0
+        0
+      else
+        sum / count
+      end
+    end.reverse
+    @teachers = Kaminari.paginate_array(@teachers).page(params[:page]).per(20)
   end
 end
