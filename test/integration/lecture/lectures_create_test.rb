ad2f6cba7112be519b5a10add45122d53e40eafb
[Cota] <[kotarouh777@hotmail.com]>
2021-02-25 10:59:41 +0900

    講義作成時テスト変更

--- test/integration/lecture/lectures_create_test.rb
+++ test/integration/lecture/lectures_create_test.rb
@@ -17,12 +17,15 @@ class LecturesCreateTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
   end
 
-  test "invalid lecture create when its teacher does not exist" do
+  test "valid lecture create when its teacher does not exist" do
     login_as(@user, scope: :user)
     get new_lecture_path
-    assert_difference 'Lecture.count', 0 do
-      post lectures_path, params: { lecture: { name:  "日本大学史", language_used: "Japanese", lecture_type: "Language", lecture_term: "spring", lecture_size: "small", group_work: "included", teacher_name: "ExampleTeacher" } } # ExampleTeacherは存在しない.
+    assert_difference 'Lecture.count', 1 do
+      post lectures_path, params: { lecture: { name:  "日本大学史", language_used: "Japanese", lecture_type: "Language", lecture_term: "spring", lecture_size: "small", group_work: "included", teacher_name: "ExampleTeacher" } } # 存在していないExampleTeacherを自動生成
     end
+    follow_redirect!
+    assert_template 'lectures/show'
+    assert_not flash.empty?
   end
 
 end
