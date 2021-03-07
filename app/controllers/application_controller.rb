5578461bd58ecafab5def2cfc0333bdb98cf147b
Ryo <matuoka2096@gmail.com>
2021-03-07 15:57:11 +0900

    言語切替機能&テスト未実装&細かい翻訳データは未登録

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :set_search
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_action :set_locale
 
   def set_search
     @q = Lecture.ransack(params[:q])
@@ -16,8 +16,20 @@ class ApplicationController < ActionController::Base
     root_path
   end
 
+  #言語切り替えようアクション
+  def change_language
+    session[:locale] = params[:language]
+    redirect_back(fallback_location: "/")
+  end
+
   protected
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:faculty,:grade,:gender])
+    end
+    # 言語切り替えようコード
+    def set_locale
+      if %w(ja en).include?(session[:locale])
+        I18n.locale = session[:locale]
+      end
     end
 end
