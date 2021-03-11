527f23b2cf8ab9d12c1b484e5ee78bfa130887af
wakachikowakachiko <mccshahaha@gmail.com>
2021-03-11 12:10:17 +0900

    bookmark fixed and padding and margin of statics pages

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,14 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
-// ここからbootstrap用のコード
-const webpack = require('webpack')
-environment.plugins.prepend(
-  'Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: 'popper.js'
-  })
-)
-// ここまでbootstrap用のコード
 module.exports = environment
