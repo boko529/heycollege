2545702f5e6618f4fad809d48c0979df9e47347d
Ryo <matuoka2096@gmail.com>
2021-03-12 22:26:08 +0900

    講義作成者と先生作成者にもポイント付与

--- test/integration/group/point_test.rb
+++ test/integration/group/point_test.rb
@@ -34,8 +34,28 @@ class Group::PointTest < ActionDispatch::IntegrationTest
     assert_difference "GroupPointHistory.count", 2 do
       assert @user.group_helpfuled_point
     end
-    # 基本である10ポイント。2つのグループに所属しているので各々に5ポイント追加(元々は10ポイント)
-    assert_equal 15, @group.group_point.current_point
-    assert_equal 15, @group.group_point.total_point
+    # 基本である100ポイント。2つのグループに所属しているので各々に50ポイント追加(元々は10ポイント)
+    assert_equal 60, @group.group_point.current_point
+    assert_equal 60, @group.group_point.total_point
+  end
+  
+  test "get helpful lecture point" do
+    # @userが2つのグループに所属しているので2つ増える
+    assert_difference "GroupPointHistory.count", 2 do
+      assert @user.group_helpfuled_lecture_point
+    end
+    # 基本である1ポイント。2つのグループに所属しているので各々に0.5ポイント追加(元々は10ポイント)
+    assert_equal 10.5, @group.group_point.current_point
+    assert_equal 10.5, @group.group_point.total_point
+  end
+
+  test "get helpful teacher point" do
+    # @userが2つのグループに所属しているので2つ増える
+    assert_difference "GroupPointHistory.count", 2 do
+      assert @user.group_helpfuled_teacher_point
+    end
+    # 基本である1ポイント。2つのグループに所属しているので各々に0.5ポイント追加(元々は10ポイント)
+    assert_equal 10.5, @group.group_point.current_point
+    assert_equal 10.5, @group.group_point.total_point
   end
 end
