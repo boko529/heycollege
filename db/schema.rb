b381d420244ef79a5318e0d3e93bfa8508f3edd6
kenji132 <ue52483@gmail.com>
2021-03-13 14:51:01 +0900

    schema修正

--- db/schema.rb
+++ db/schema.rb
@@ -57,7 +57,6 @@ ActiveRecord::Schema.define(version: 2021_03_11_024022) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.text "profile"
   end
 
   create_table "helpfuls", force: :cascade do |t|
@@ -182,16 +181,6 @@ ActiveRecord::Schema.define(version: 2021_03_11_024022) do
     t.text "message"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  create_table "zooms", force: :cascade do |t|
-    t.text "host_url"
-    t.text "join_url"
-    t.integer "user_id"
-    t.text "title"
-    t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "bookmarks", "lectures"
