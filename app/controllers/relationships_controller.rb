c99028d216df7797cb3a9c49d7745fcbd446a11c
Ryo <matuoka2096@gmail.com>
2021-03-14 19:37:38 +0900

    ブックマークは非同期に変更

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -5,13 +5,15 @@ class RelationshipsController < ApplicationController
     current_user.follow(params[:id])
     @user = User.find(params[:id])
     current_user.create_notification_follow(@user)
-    redirect_to user_path(@user.id)
+    # 非同期通信後でページ遷移(redirect_to)があるとうまくいかない(turbolinksをoffにしている場合)
+    # redirect_to user_path(@user.id)
   end
 
   # アンフォローする
   def unfollow
     current_user.unfollow(params[:id])
     @user = User.find(params[:id])
-    redirect_to user_path(@user.id)
+    # 非同期通信後でページ遷移(redirect_to)があるとうまくいかない(turbolinksをoffにしている場合)
+    # redirect_to user_path(@user.id)
   end
 end
