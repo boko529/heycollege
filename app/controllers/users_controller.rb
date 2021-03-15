ae6d6018bec22c1170487dd365bfc48ea1b52aff
kenji132 <ue52483@gmail.com>
2021-03-15 16:47:00 +0900

    サインインしていないユーザーはユーザー詳細ページを見れない

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController   
-  before_action :authenticate_user!, only: [:edit,:update]
+  before_action :authenticate_user!, only: [:edit,:update,:show,:following,:follower]
   def show
     @user = User.find(params[:id])
     @reviews = @user.reviews
