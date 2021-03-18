8736c19d39126947cf35bdd7f7af18ed996979a4
kenji132 <ue52483@gmail.com>
2021-03-18 11:48:26 +0900

    招待URLを用いてzoom作成

--- app/controllers/zooms_controller.rb
+++ app/controllers/zooms_controller.rb
@@ -1,33 +1,34 @@
 class ZoomsController < ApplicationController
   def create
-    @api_key = "66aoWxi3R1CCqYtfXXX3vA"
-    @secret  = "eoEOrBqOAFBidWwGKmrB2HUHwb6YT8a6DRSV"
-    @jwt = self.GenerateJWT
-    @user_id = self.GetUserData(@jwt)
-    @meeting_url = "https://api.zoom.us/v2/users/#{@user_id}/meetings"
+    # APIを用いてzoom作成
+    # @api_key = "66aoWxi3R1CCqYtfXXX3vA"
+    # @secret  = "eoEOrBqOAFBidWwGKmrB2HUHwb6YT8a6DRSV"
+    # @jwt = self.GenerateJWT
+    # @user_id = self.GetUserData(@jwt)
+    # @meeting_url = "https://api.zoom.us/v2/users/#{@user_id}/meetings"
 
-    uri = URI.parse(@meeting_url)
-    http = Net::HTTP.new(uri.host, uri.port)
+    # uri = URI.parse(@meeting_url)
+    # http = Net::HTTP.new(uri.host, uri.port)
 
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    # http.use_ssl = true
+    # http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-    req = Net::HTTP::Post.new(uri.path)
-    req["Authorization"] = "Bearer #{@jwt}"
-    aa = "Bearer #{@jwt}"
-    puts aa
-    req["Content-Type"] = "application/json"
-    req.body = {
-        "type":1,
-        }.to_json
-    res = http.request(req)
-    parseURL = JSON.parse(res.body)
+    # req = Net::HTTP::Post.new(uri.path)
+    # req["Authorization"] = "Bearer #{@jwt}"
+    # aa = "Bearer #{@jwt}"
+    # puts aa
+    # req["Content-Type"] = "application/json"
+    # req.body = {
+    #     "type":1,
+    #     }.to_json
+    # res = http.request(req)
+    # parseURL = JSON.parse(res.body)
 
-
+    # 招待URLを用いてzoom作成
     @zoom = Zoom.new(zoom_params)
     @zoom.user_id = current_user.id
-    @zoom.host_url = parseURL["start_url"]
-    @zoom.join_url = parseURL["join_url"]
+    # @zoom.host_url = parseURL["start_url"]
+    # @zoom.join_url = parseURL["join_url"]
     if @zoom.save
       flash[:notice] = "zoomの部屋が作成されました"
       redirect_to zoom_path(@zoom.id)
@@ -73,36 +74,37 @@ class ZoomsController < ApplicationController
     end
   end
 
-  require 'net/https'
-  require 'net/http'
-  require 'uri'
-  require 'json'
+  # require 'net/https'
+  # require 'net/http'
+  # require 'uri'
+  # require 'json'
 
-  def GenerateJWT
-    payload = { 
-        iss: @api_key,
-        exp: Time.now.to_i + 4 * 36000
-    }
-    token = JWT.encode payload, @secret, 'HS256'
-    return token
-  end
+  # def GenerateJWT
+  #   payload = { 
+  #       iss: @api_key,
+  #       exp: Time.now.to_i + 4 * 36000
+  #   }
+  #   token = JWT.encode payload, @secret, 'HS256'
+  #   return token
+  # end
 
-  def GetUserData(_token)
-    uri = URI.parse("https://api.zoom.us/v2/users")
-    request = Net::HTTP::Get.new(uri)
-    request["Authorization"] = "Bearer "+_token
-    req_options = {
-        use_ssl: uri.scheme == "https",
-    }
-    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-        http.request(request)
-    end
-    result = JSON.parse(response.body)
-    return result["users"][0]["id"].to_s
-  end
+  # def GetUserData(_token)
+  #   uri = URI.parse("https://api.zoom.us/v2/users")
+  #   request = Net::HTTP::Get.new(uri)
+  #   request["Authorization"] = "Bearer "+_token
+  #   req_options = {
+  #       use_ssl: uri.scheme == "https",
+  #   }
+  #   response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+  #       http.request(request)
+  #   end
+  #   result = JSON.parse(response.body)
+  #   return result["users"][0]["id"].to_s
+  # end
 
   private
   def zoom_params
-    params.require(:zoom).permit(:join_url,:host_url, :user_id, :title, :description)
+    params.require(:zoom).permit(:join_url, :user_id, :title, :description)
+    # params.require(:zoom).permit(:join_url,:host_url, :user_id, :title, :description)
   end
 end
