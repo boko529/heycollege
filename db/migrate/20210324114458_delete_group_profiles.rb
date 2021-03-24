80d68ee6d040efb0f3fcbd084b2ad1c6b27135e8
[Cota] <[kotarouh777@hotmail.com]>
2021-03-24 22:42:37 +0900

    修正しました

--- db/migrate/20210324114458_delete_group_profiles.rb
+++ db/migrate/20210324114458_delete_group_profiles.rb
@@ -1,5 +1,7 @@
 class DeleteGroupProfiles < ActiveRecord::Migration[6.1]
   def change
+    # group_profileテーブル削除します.
+    # 代わりにgroupテーブルにprofile(text型)カラムを追加してます.
     drop_table :group_profiles
   end
 end
