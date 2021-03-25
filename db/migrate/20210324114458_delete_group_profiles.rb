f3f604a89d41d5039196331b0c9743ec86e6bab5
Ryo <matuoka2096@gmail.com>
2021-03-25 13:59:30 +0900

    migrationファイル一つ削除

--- db/migrate/20210324114458_delete_group_profiles.rb
+++
@@ -1,7 +0,0 @@
-class DeleteGroupProfiles < ActiveRecord::Migration[6.1]
-  def change
-    # group_profileテーブル削除します.
-    # 代わりにgroupテーブルにprofile(text型)カラムを追加してます.
-    drop_table :group_profiles
-  end
-end
