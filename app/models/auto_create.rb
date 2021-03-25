543a127fe746c3502c6dfb1a7c2b5bd10387101c
Ryo <matuoka2096@gmail.com>
2021-03-25 21:56:33 +0900

    製作途中,

--- app/models/auto_create.rb
+++ app/models/auto_create.rb
@@ -8,14 +8,16 @@ class AutoCreate < ApplicationRecord
     #   "teacher_j" => 'teacher_name'
     # }
     CSV.foreach(file.path, headers: true).with_index(2) do |row, row_number|
-      lecture_name = row["lecture_j"]
-      teacher_name = row["teacher_j"]
-      if teacher = Teacher.find_by(name: teacher_name)
-        teacher.lectures.create!(name: lecture_name, user_id: 1) # 失敗した際にエラーが出るようにする必要あり、最後にセーブ持っていってトランザクションまとめるのもあり。
-      else
-        teacher = Teacher.new(name: teacher_name, user_id: 1)
-        teacher.save! # if文を用いたほうが良い？
-        teacher.lectures.create!(name: lecture_name)
+      @teacher_name = row["先生名"]
+      @lecture_name = row["講義名"]
+      if @lecture_name
+        if teacher = Teacher.find_by(name: @teacher_name)
+          Lecture.create!(name: @lecture_name, user_id: 1, teacher_id: teacher.id) # 失敗した際にエラーが出るようにする必要あり、最後にセーブ持っていってトランザクションまとめるのもあり。
+        else
+          teacher = Teacher.new(name: @teacher_name, user_id: 1)
+          teacher.save! # if文を用いたほうが良い？
+          Lecture.create!(name: @lecture_name, user_id: 1, teacher_id: teacher.id)
+        end
       end
     end
   end
