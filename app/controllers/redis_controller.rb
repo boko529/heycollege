680df3876d826686d03e117ff41b1016c7a83764
[Cota] <[kotarouh777@hotmail.com]>
2021-03-27 18:55:59 +0900

    adminがランキングを更新できるようにした.

--- app/controllers/redis_controller.rb
+++ app/controllers/redis_controller.rb
@@ -1,25 +1,33 @@
-# このファイルはサンプル用.すぐ消します.
+# 管理者用.ランキングを更新できます.
 class RedisController < ApplicationController
-    def show
-      REDIS.set('mykey', 'Hello')
-    end
+  before_action :authenticate_user!
+  before_action :if_not_admin
 
-    def ranking_update
-      Lecture.all.map{ |lecture| REDIS.zadd "lectures/rank", lecture.average_score, lecture.id }
-      Teacher.all.map{ |teacher| REDIS.zadd "teachers/rank", teacher.average_score, teacher.id }
-      #ユーザーランキング処理(管理者、退会者は除く,非承認者)
-      # ↓なぜかこの書き方（map）にしたらいけた. ちょい見栄え悪いかもやけど許して.
-      User.where(admin: false, is_deleted: false).where.not(confirmed_at: nil).includes(:user_point_history).map{ |user|
-        total_amount = 0
-        if user.user_point_history.present?
-          user.user_point_history.all.each do |user_point_history|
-            if user_point_history.created_at.month == Time.now.month
-              total_amount += user_point_history.amount
-            end
+  def show
+  end
+
+  def ranking_update
+    Lecture.all.map{ |lecture| REDIS.zadd "lectures/rank", lecture.average_score, lecture.id }
+    Teacher.all.map{ |teacher| REDIS.zadd "teachers/rank", teacher.average_score, teacher.id }
+    #ユーザーランキング処理(管理者、退会者は除く,非承認者)
+    # ↓なぜかこの書き方（map）にしたらいけた. ちょい見栄え悪いかもやけど許して.
+    User.where(admin: false, is_deleted: false).where.not(confirmed_at: nil).includes(:user_point_history).map{ |user|
+      total_amount = 0
+      if user.user_point_history.present?
+        user.user_point_history.all.each do |user_point_history|
+          if user_point_history.created_at.month == Time.now.month
+            total_amount += user_point_history.amount
           end
         end
-        REDIS.zadd "users/rank/#{Time.now.month.to_s}", total_amount, user.id
-      }
-      render 'show'
-    end
+      end
+      REDIS.zadd "users/rank/#{Time.now.month.to_s}", total_amount, user.id
+    }
+    flash[:success] = "ランキングを更新しました"
+    render 'show'
+  end
+
+  private
+  def if_not_admin
+    redirect_to root_path unless current_user.admin?
+  end
   end
