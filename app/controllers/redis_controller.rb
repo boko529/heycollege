9a1897a0852699cee1feaec21e0192f2d934263b
[Cota] <[kotarouh777@hotmail.com]>
2021-03-28 09:50:46 +0900

    修正

--- app/controllers/redis_controller.rb
+++ app/controllers/redis_controller.rb
@@ -7,8 +7,8 @@ class RedisController < ApplicationController
   end
 
   def ranking_update
-    Lecture.all.map{ |lecture| REDIS.zadd "lectures/rank", lecture.average_score, lecture.id }
-    Teacher.all.map{ |teacher| REDIS.zadd "teachers/rank", teacher.average_score, teacher.id }
+    Lecture.all.map{ |lecture| REDIS.zadd "rank/lectures", lecture.average_score, lecture.id }
+    Teacher.all.map{ |teacher| REDIS.zadd "rank/teachers", teacher.average_score, teacher.id }
     #ユーザーランキング処理(管理者、退会者は除く,非承認者)
     # ↓なぜかこの書き方（map）にしたらいけた. ちょい見栄え悪いかもやけど許して.
     User.where(admin: false, is_deleted: false).where.not(confirmed_at: nil).includes(:user_point_history).map{ |user|
@@ -20,13 +20,14 @@ class RedisController < ApplicationController
           end
         end
       end
-      REDIS.zadd "users/rank/#{Time.now.month.to_s}", total_amount, user.id
+      REDIS.zadd "rank/users/#{Time.now.month.to_s}", total_amount, user.id
     }
     flash[:success] = "ランキングを更新しました"
     render 'show'
   end
 
   private
+
   def if_not_admin
     redirect_to root_path unless current_user.admin?
   end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     #講義ランキング処理
+    # もともとのコード
     # lectures = Lecture.includes(:reviews).sort_by do |lecture|
     #   if lecture.reviews.present?
     #     lecture.average_score
@@ -9,10 +10,12 @@ class StaticPagesController < ApplicationController
     #   end
     # end.reverse  ids.map{ |id| Article.find(id) }
     
-    lectures = REDIS.zrevrangebyscore( "lectures/rank", "+inf", "-inf" ).map{ |id| Lecture.find(id) }
+    # redis用のコード
+    lectures = REDIS.zrevrangebyscore( "rank/lectures", "+inf", "-inf" ).map{ |id| Lecture.find(id) }
     @lectures = Kaminari.paginate_array(lectures).page(params[:lectures_page]).per(20)
 
     #先生ランキング処理
+    # もともとのコード
     # teachers = Teacher.includes(lectures: :reviews).sort_by do |teacher|
     #   if teacher.lectures.present?
     #     teacher.average_score
@@ -21,10 +24,12 @@ class StaticPagesController < ApplicationController
     #   end
     # end.reverse
 
-    teachers = REDIS.zrevrangebyscore( "teachers/rank", "+inf", "-inf" ).map{ |id| Teacher.find(id) }
+    # redis用のコード
+    teachers = REDIS.zrevrangebyscore( "rank/teachers", "+inf", "-inf" ).map{ |id| Teacher.find(id) }
     @teachers = Kaminari.paginate_array(teachers).page(params[:teachers_page]).per(20)
     
     #ユーザーランキング処理(管理者、退会者は除く,非承認者)
+    # もともとのコード
     # users = User.where(admin: false, is_deleted: false).where.not(confirmed_at: nil).includes(:user_point_history).sort_by  do |user|
     #   total_amount = 0
     #   if user.user_point_history.present?
@@ -37,7 +42,8 @@ class StaticPagesController < ApplicationController
     #   total_amount
     # end.reverse
 
-    users = REDIS.zrevrangebyscore( "users/rank/#{Time.now.month.to_s}", "+inf", "-inf" ).map{ |id| User.find(id) }
+    # redis用のコード
+    users = REDIS.zrevrangebyscore( "rank/users/#{Time.now.month.to_s}", "+inf", "-inf" ).map{ |id| User.find(id) }
     @users = Kaminari.paginate_array(users).page(params[:users_page]).per(20)
 
     #最新のニュースを表示
