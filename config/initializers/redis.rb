77e05fb7e4d41608d94ba6afaf7b76d906ffc6cb
[Cota] <[kotarouh777@hotmail.com]>
2021-03-28 20:57:44 +0900

    本番環境(heroku)の設定

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,5 @@
 require 'redis'
 uri = URI.parse('localhost:6379')
 REDIS = Redis.new(host: uri.host, port: uri.port) # ここもしかすると環境違えばエラー起こるかも.
+# ↓ 本番環境(heroku)ではこっち使ってください.
+# REDIS = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
