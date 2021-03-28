47680b1893a7fb02c3f01010193d470ef7d5ffb1
kenji132 <ue52483@gmail.com>
2021-03-28 20:55:01 +0900

    zoom作成フォーム修正

--- app/models/zoom.rb
+++ app/models/zoom.rb
@@ -1,7 +1,7 @@
 class Zoom < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
-  validates :description, presence: true
+  # validates :description, presence: true
   validates :start_time, presence: true
   validates :end_time, presence: true
   VALID_JOINURL_REGEX = /\A^(http|https):\/\/*(us04web\.)?zoom\.us\/j\/(.*)?$\z/ix
