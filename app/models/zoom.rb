eff3b8b52d66545439cc9bb89491b3e06cafcedb
kenji132 <ue52483@gmail.com>
2021-03-30 10:21:42 +0900

    zoom編集機能修正

--- app/models/zoom.rb
+++ app/models/zoom.rb
@@ -6,4 +6,14 @@ class Zoom < ApplicationRecord
   validates :end_time, presence: true
   VALID_JOINURL_REGEX = /\A^(http|https):\/\/*(us04web\.)?zoom\.us\/j\/(.*)?$\z/ix
   validates :join_url, presence: true,format: { with: VALID_JOINURL_REGEX }
+  validate :start_end_check
+  validate :start_check
+
+  def start_end_check
+    errors.add(:end_time, "は開始時刻より遅い時間を選択してください") if self.start_time > self.end_time
+  end
+
+  def start_check
+    errors.add(:start_time, "は現在の日時より遅い時間を選択してください") if self.start_time < Time.now
+  end
 end
