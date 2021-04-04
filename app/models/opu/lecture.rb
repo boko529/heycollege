17a8d2d2fd9585905641f148a68acd5436c78d06
Ryo <matuoka2096@gmail.com>
2021-04-04 15:36:27 +0900

    dependent,destroy追加

--- app/models/opu/lecture.rb
+++ app/models/opu/lecture.rb
@@ -1,5 +1,5 @@
 class Opu::Lecture < Lecture
   enum lecture_lang: { none: 0, ja: 1, en: 2, ko: 4, zh: 5, fr: 10, de: 11}, _prefix: :true # es=>スペイン語, ko=>韓国語, zh=>中国語, ms=>マレーシア語＋インドネシア語, th=>タイ語, vi=>ベトナム語, ej=>日本語英語兼用, fr => フランス語, de => ドイツ語
   enum field: { APS: 1, APM: 2, APSAPM: 3, Liberal: 4, Language: 5}, _prefix: :true
-  has_many :lecture_infos
+  has_many :lecture_infos, dependent: :destroy # lectureが消えたらlecture_infoも消える。
 end
