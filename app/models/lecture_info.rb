103f7d09c9b5ffe467fbe5318c91c6c29cb4af08
Ryo <matuoka2096@gmail.com>
2021-04-04 16:40:04 +0900

    LectureInfoの翻訳追加

--- app/models/lecture_info.rb
+++ app/models/lecture_info.rb
@@ -1,7 +1,7 @@
 class LectureInfo < ApplicationRecord
   enum faculty: { Gensisu: 1, Kougaku: 2, Seikan: 3, Tiiki: 4 }, _prefix: :true 
-  enum department: { denden: 0, bukka: 1, kikai: 2, juuui: 3, ousei: 4, ryokuti: 5, rigaku: 6, kanngo: 7, rihabiri: 8, kyouhuku: 9, tijou: 10,kanshisu: 11,maneji: 12, }, _prefix: :true 
-  enum major: { joukou: 0, dennsisu: 1, denbutu: 2, ouka: 3, kakou: 4, koukuu: 5, kaiyou: 6, kikaikou: 7, seimei: 8, shokubutu: 9, suuri: 10, buturi: 11, bunshi: 12, seibutu: 13, rigakuryou: 14,sagyouryou: 15, eiyouryou: 16, kankyou: 17,shokyou: 18,ninkan: 19,manejika: 20,keizai: 21}, _prefix: :true 
+  enum department: { denden: 0, bukka: 1, kikai: 2, juui: 3, ousei: 4, ryokuti: 5, rigaku: 6, kango: 7, rihabiri: 8, kyouhuku: 9, tijou: 10,kanshisu: 11,maneji: 12, }, _prefix: :true 
+  enum major: { joukou: 0, densisu: 1, denbutu: 2, ouka: 3, kakou: 4, material: 5, koukuu: 6, kaiyou: 7, kikaikou: 8, seimei: 9, shokubutu: 10, suuri: 11, buturi: 12, bunshi: 13, seibutu: 14, rigakuryou: 15,sagyouryou: 16, eiyouryou: 17, kankyou: 18,shakyou: 19,ninkan: 20}, _prefix: :true 
   enum day_of_week: { sun: 0, mon: 1, teu: 2, wed: 3, thu: 4, fri: 5, stu: 6}, _prefix: :true 
   enum semester: { fir_seme: 0, sec_seme: 1}, _prefix: :true 
   enum period: { first: 0, second: 1, third: 2, fourth: 3, fifth: 4, sixth: 5}, _prefix: :true 
--- app/models/opu/lecture.rb
+++ app/models/opu/lecture.rb
@@ -1,5 +1,6 @@
 class Opu::Lecture < Lecture
-  enum lecture_lang: { none: 0, ja: 1, en: 2, ko: 4, zh: 5, fr: 10, de: 11}, _prefix: :true # es=>スペイン語, ko=>韓国語, zh=>中国語, ms=>マレーシア語＋インドネシア語, th=>タイ語, vi=>ベトナム語, ej=>日本語英語兼用, fr => フランス語, de => ドイツ語
-  enum field: { APS: 1, APM: 2, APSAPM: 3, Liberal: 4, Language: 5}, _prefix: :true
+  # enum lecture_lang: { none: 0, ja: 1, en: 2, ko: 4, zh: 5, fr: 10, de: 11}, _prefix: :true # es=>スペイン語, ko=>韓国語, zh=>中国語, ms=>マレーシア語＋インドネシア語, th=>タイ語, vi=>ベトナム語, ej=>日本語英語兼用, fr => フランス語, de => ドイツ語
+  enum field: { Syozemi: 1, Pankyo: 2, English: 3, Shogai: 4, Gaikokugotokubetu: 5, Kaigaikensyu: 6, Kenkou: 7, Zyouhou: 8, Iryo: 9, Senmon: 10, Tokurei: 11, Sikaku: 12, Rikeikiso: 13}, _prefix: :true
+  # Syozemi=>初ゼミ,Pankyo=>一般教養, English=>外国語科目(英語), Shogai=>初習外国語, Gaikokugotokubetu: 外国語特別, Kaigaikensyu: 海外研修, Kenkou: 健康スポーツ・科学科目, Zyouhou: 情報基礎, Iryo: 医療保険, Senmon: 専門科目, Tokurei: 特例, Sikaku: 資格, Rikeikiso: 理系基礎科目
   has_many :lecture_infos, dependent: :destroy # lectureが消えたらlecture_infoも消える。
 end
