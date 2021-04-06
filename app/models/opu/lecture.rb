912463369d44f097940b3619a91b5650eb14fa7b
Ryo <matuoka2096@gmail.com>
2021-04-06 10:12:42 +0900

    検索機能のエラーを削除

--- app/models/opu/lecture.rb
+++ app/models/opu/lecture.rb
@@ -3,5 +3,4 @@ class Opu::Lecture < Lecture
   enum field: { Syozemi: 1, Pankyo: 2, English: 3, Shogai: 4, Gaikokugotokubetu: 5, Kaigaikensyu: 6, Kenkou: 7, Zyouhou: 8, Iryo: 9, Senmon: 10, Tokurei: 11, Sikaku: 12, Rikeikiso: 13}, _prefix: :true
   enum state: { Online: 1, Offline: 2, Complex: 3, Unknown: 4} # 授業形式がオンラインかオフラインか混合か
   # Syozemi=>初ゼミ,Pankyo=>一般教養, English=>外国語科目(英語), Shogai=>初習外国語, Gaikokugotokubetu: 外国語特別, Kaigaikensyu: 海外研修, Kenkou: 健康スポーツ・科学科目, Zyouhou: 情報基礎, Iryo: 医療保険, Senmon: 専門科目, Tokurei: 特例, Sikaku: 資格, Rikeikiso: 理系基礎科目
-  has_many :lecture_infos, dependent: :destroy # lectureが消えたらlecture_infoも消える。
 end
